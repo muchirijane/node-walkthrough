b86032be506250ef4870b9e98af094022fc0160a
jane muthoni <muchirijane@outlook.com>
2020-08-02 23:07:09 +0300

    creating a web server

--- node-farm/index.js
+++ node-farm/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
-
+const http = require('http');
+//////////////////////////////////////////////////// file system
 //synchronous method, blocking
 // const text = fs.readFileSync('./txt/input.txt', 'utf8');
 // console.log(text);
@@ -9,18 +10,30 @@ const fs = require('fs');
 // console.log('Created textOutput file');
 
 //asynchronous method/ non-blocking method
-fs.readFile('./txt/starty.txt', 'utf-8' ,(err, data1)=>{
-    if(err) return console.log('Error!!! 💣 ');
-    console.log(data1);
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8' ,(err, data2)=>{
-        console.log(data2);
-        fs.readFile('./txt/append.txt', 'utf-8', (err, data3)=>{
-            console.log(data3);
+// fs.readFile('./txt/starty.txt', 'utf-8' ,(err, data1)=>{
+//     if(err) return console.log('Error!!! 💣 ');
+//     console.log(data1);
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8' ,(err, data2)=>{
+//         console.log(data2);
+//         fs.readFile('./txt/append.txt', 'utf-8', (err, data3)=>{
+//             console.log(data3);
 
-            fs.writeFile('./txt/final-output.txt', `${data2} \n${data3}`, 'utf-8', (err)=>{
-                console.log('The file has been written 😏');
-            });
-        });
-    });
+//             fs.writeFile('./txt/final-output.txt', `${data2} \n${data3}`, 'utf-8', (err)=>{
+//                 console.log('The file has been written 😏');
+//             });
+//         });
+//     });
+// });
+// console.log('This will be first! 🔥 ');
+
+/////////////////////////////////////////////////////////////////////////////
+//////***********************  server.js *********//////////////////////////
+const server = http.createServer((req, res) => {
+    console.log(req);
+    res.end('Hello from server! 😍');
 });
-console.log('This will be first! 🔥 ');
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Server listening! 💣');
+});
+
